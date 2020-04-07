ee1bf9952fe875c909bc31694d9cd156bbb8bc3a
themindstorm <parth.kabra@gmail.com>
2020-04-07 15:51:32 +0800

    added icons

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,19 @@ module.exports = {
     msTileColor: '#ffffff',
     backgroundColor: '#aaaaff',
 
-    appleMobileWebAppCapable: true,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#ffffff',
 
     manifestOptions: {
       display: 'standalone',
       background_color: '#aaaaff',
-    }
+    },
+
+    // iconPaths: {
+    //   favicon32: 'img/icons/favicon-32x32.png',
+    //   favicon16: 'img/icons/favicon-16x16.png',
+    //   appleTouchIcon: 'img/icons/apple-touch-icon.png',
+    //   maskIcon: 'img/icons/favicon-16x16.png',
+    // }
   }
 }
